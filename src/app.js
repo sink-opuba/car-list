2d9091b534c1e32526362514cd27160f3b2318f8
sink-opuba <osinkopere@yahoo.com>
2019-09-17 11:06:59 +0100

    populated database with starter data

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-const $ = require('jquery');
-console.log($);
+$(document).ready(function() {
+  const displayResult = ([car]) => {
+    if(car.id === 1) {
+        const img = document.querySelector("img");
+        img.src = car.imageUrl;
+    }
+  }
+  const Url = "http://localhost:3000/cars";
+  $.ajax({
+    url: Url,
+    type: "GET",
+    success: function(result) {
+      displayResult(result);
+    //   console.log(result);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+});
