e7445ede07d91f5b3628df230e48bd33cfa66fea
sink-opuba <osinkopere@yahoo.com>
2019-09-20 08:49:03 +0100

    fix login validation bug

--- src/js/login.js
+++ src/js/login.js
@@ -1,71 +1,77 @@
 $(document).ready(function() {
-    const BASE_URI = "http://localhost:3000";
-    const emailInput = $("#email");
-    const passwordInput = $("#password");
-    
-    //LOGIN
-    $(".btn-login").click(function(event) {
-        event.preventDefault(); 
-        const data = {
-            email: emailInput.val(),
-            password: passwordInput.val(),
-        }       
-        $(function() {
-            $.ajax({
-                url: `${BASE_URI}/users`,
-                method: "GET",
-                success: function (users) {
-                    const user = users.some(user => user.email == data.email && user.password == data.password);
-                    if (user) {
-                        window.location.replace("add_new_vehicle.html");
-                        localStorage.setItem('email', `${data.email}`);
-                        
-                    } else {
-                        $('.login-info').css('display', 'block');
-                        return;
-                    }
-                },
-                error: function (err) {
-                    console.log("Error: ", err);
-                }
-            });
-        }); 
-    });
-        // /SIGN UP
-        $(".btn-signup").click(function(event) {
-            event.preventDefault();
-            const data = {
-                email: emailInput.val(),
-                password: passwordInput.val(),
-                isAdmin: false
-            };
-            
-            $(function() {
-                $.ajax({
-                    url: `${BASE_URI}/users`,
-                    method: "GET",
-                    success: function(users) {
-                        const user = users.some(user => user.email == data.email);
-                        //If user already exists    
-                        if (user) {
-                            $('.signup-info').css('display', 'block');
-                            return;
-                        };
-                        //New User - Add to DB   
-                        $.ajax({
-                            url: `${BASE_URI}/users`,
-                            method: "POST",
-                            data,
-                            success: function() {
-                                window.location.replace("add_new_vehicle.html");
-                                localStorage.setItem('email', `${data.email}`);
-                            }
-                        });
-                    },
-                    error: function(err) {
-                        console.log("Error: ", err);
-                    }
-                });
-            });
+  const BASE_URI = "http://localhost:3000";
+  const emailInput = $("#email");
+  const passwordInput = $("#password");
+
+  //LOGIN
+  $(".btn-login").click(function(event) {
+    event.preventDefault();
+    const data = {
+      email: emailInput.val(),
+      password: passwordInput.val()
+    };
+    if (data.email && data.password != "") {
+      $(function() {
+        $.ajax({
+          url: `${BASE_URI}/users`,
+          method: "GET",
+          success: function(users) {
+            const user = users.some(
+              user => user.email == data.email && user.password == data.password
+            );
+            if (user) {
+              window.location.replace("add_new_vehicle.html");
+              localStorage.setItem("email", `${data.email}`);
+            } else {
+              $(".login-info").css("display", "block");
+              return;
+            }
+          },
+          error: function(err) {
+            console.log("Error: ", err);
+          }
         });
-    });
+      });
+    }
+  });
+  // /SIGN UP
+  $(".btn-signup").click(function(event) {
+    event.preventDefault();
+    const data = {
+      email: emailInput.val(),
+      password: passwordInput.val(),
+      isAdmin: false
+    };
+
+    //Make sure user input is not empty
+    if (data.email && data.password != "") {
+      $(function() {
+        $.ajax({
+          url: `${BASE_URI}/users`,
+          method: "GET",
+          success: function(users) {
+            const user = users.some(user => user.email == data.email);
+            //If user already exists
+            if (user) {
+              $(".signup-info").css("display", "block");
+              return;
+            }
+            //New User - Add to DB
+            $.ajax({
+              url: `${BASE_URI}/users`,
+              method: "POST",
+              data,
+              success: function() {
+                window.location.replace("add_new_vehicle.html");
+                localStorage.setItem("email", `${data.email}`);
+              }
+            });
+          },
+          error: function(err) {
+            console.log("Error: ", err);
+          }
+        });
+      });
+    }
+  });
+});
